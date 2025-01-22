a23589c5eb3237ce14a6a327b9937011cfe12b23
Kushal Goyal <goyalkushal04@gmail.com>
2025-01-22 14:11:03 +0000

    improvement: utils

--- packages/utils/src/getUserInfo.ts
+++ packages/utils/src/getUserInfo.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-const db = new PrismaClient();
+import { db } from "@kushal/prisma";
 
 export const getUserInfo = async (userId: string) => {
     try {
